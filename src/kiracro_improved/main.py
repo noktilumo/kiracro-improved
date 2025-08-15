36ed9e38ad8a145a7cdd7af9c42f3f91d6722cdc
noktilumo <noktilumo@gmail.com>
2025-08-14 21:31:27 -0400

    ahwwhakqq!????? good

--- src/kiracro_improved/main.py
+++ src/kiracro_improved/main.py
@@ -1,3 +1,10 @@
-def main():
-    print("Welcome to kiracro-improved!")
+import click
+from kiracro_improved.runner import run
 
+@click.command()
+@click.option('--holdable', type=bool, default=False, help='Hold to macro')
+def main(holdable):
+    run(holdable)
+
+if __name__ == "__main__":
+    main()
---
+++ src/kiracro_improved/runner.py
@@ -0,0 +1,66 @@
+from time import sleep
+from pynput import mouse, keyboard
+import os
+import platform
+
+mouse_controller = mouse.Controller()
+
+hold = False
+can_run = True
+running = False
+
+def clear_terminal():
+    print("\033[2J\033[H", end="")  # Clears screen and moves cursor to top-left
+
+
+def print_status():
+    clear_terminal()
+    print(f"[FLAGS]\nholdable {hold}")
+    print(f"[STATES]\nrunning {running}")
+
+def macro():
+    mouse_controller.scroll(0, 1)
+    sleep(0.01)
+
+    mouse_controller.scroll(0, -1)
+    sleep(0.01)
+
+def on_press(key):
+    global running, can_run, hold
+
+    if key == keyboard.Key.esc:
+        print("Pressed esc, exiting...")
+        can_run = not can_run
+
+    try:
+        if key.char == 't':
+            if hold:
+                running = True
+                return
+            else:
+                running = not running
+
+    except Exception:
+        pass
+
+def on_release(key):
+    global running
+    try:
+        if key.char == 't':
+            if hold:
+                running = False
+    except Exception:
+        pass
+
+def run(holdable: bool):
+    global hold
+    hold = holdable
+    with keyboard.Listener(
+        on_press=on_press,
+        on_release=on_release) as listener:
+        while can_run:
+            print_status()
+            if running:
+                macro()
+            sleep(0.1)
+            
