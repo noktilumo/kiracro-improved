af3999c091d9a8b3a7739469492a8f95155c4520
noktilumo <noktilumo@gmail.com>
2025-08-15 01:55:56 -0400

    added variable for keybind

--- src/kiracro_improved/runner.py
+++ src/kiracro_improved/runner.py
@@ -8,14 +8,17 @@ mouse_controller = mouse.Controller()
 hold = False
 can_run = True
 running = False
+keybind = 'q'
+
 
 def clear_terminal():
     print("\033[2J\033[H", end="")  # Clears screen and moves cursor to top-left
 
 
 def print_status():
+    global running, keybind, hold
     clear_terminal()
-    print(f"[FLAGS]\nholdable {hold}")
+    print(f"[FLAGS]\nholdable {hold}\nkeybind {keybind}")
     print(f"[STATES]\nrunning {running}")
 
 def macro():
@@ -26,14 +29,14 @@ def macro():
     sleep(0.01)
 
 def on_press(key):
-    global running, can_run, hold
+    global running, can_run, hold, keybind
 
     if key == keyboard.Key.esc:
         print("Pressed esc, exiting...")
         can_run = not can_run
 
     try:
-        if key.char == 't':
+        if key.char == keybind:
             if hold:
                 running = True
                 return
@@ -46,7 +49,7 @@ def on_press(key):
 def on_release(key):
     global running
     try:
-        if key.char == 't':
+        if key.char == keybind:
             if hold:
                 running = False
     except Exception:
